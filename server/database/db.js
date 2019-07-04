9e8293961e930d3658b438422a6bb4af6f4095ab
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-07-05 09:47:22 +1200

    ready to deploy to heroku hopefully

--- server/database/db.js
+++ server/database/db.js
@@ -1,15 +1,15 @@
 const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
+const config = require('../../knexfile.js')[environment]
 const connection = require('knex')(config)
 
 
-function getPlayers(db = connection){
+function getPlayers(db = connection) {
     console.log("reaching db")
     return db('players').select()
-    
+
 
 }
- module.exports = {
-     getPlayers,
- }
+module.exports = {
+    getPlayers,
+}
 
